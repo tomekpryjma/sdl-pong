90877eeb5b9c999daf27ccea62351cc07db532cd
Tomek Pryjma <tomek.pryjma@gmail.com>
2023-07-10 20:54:41 +0100

    Import scoring & initiate srand()

--- sdl-pong/main.cpp
+++ sdl-pong/main.cpp
@@ -2,9 +2,13 @@
 #include "Paddle.h"
 #include "Player.h"
 #include "Ball.h"
+#include "Scoring.h"
 
 int main(int argc, char* args[]) {
+	srand(time(NULL));
+
 	App app;
+	Scoring scoring;
 	int midPointY = HEIGHT / 2;
 	std::shared_ptr<Player> pPlayer = std::shared_ptr<Player>(
 		new Player(20, midPointY - PADDLE_HEIGHT / 2, app.getRenderer()));
@@ -19,7 +23,7 @@ int main(int argc, char* args[]) {
 		app.getRenderer()
 	);
 
-	app.loop(pPlayer, pPaddle, ball);
+	app.loop(pPlayer, pPaddle, ball, scoring);
 	app.quit();
 	return 0;
 }
